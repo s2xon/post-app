73de3d2ae52f438febe8788fe5f4d3110267bba1
Saxon Payne <saxattack101@gmail.com>
2024-04-16 20:36:27 -0700

    eat: Added a Hamburger component for mobile navigation menu 🍔

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,19 +3,52 @@ import NavButton from "./ui/NavButton";
 import LogoText from "./ui/LogoText";
 import AuthButtons from "./AuthButtons";
 
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Button } from "./ui/button";
+
 const Nav = () => {
 	return (
-		<nav className="min-w-[100vw] min-h-20 flex justify-between items-center border-b border-white border-opacity-25 fixed">
-			<LogoText className={"cursor-pointer mx-6 min-h-9 max-h-9 min-w-28 max-w-28"} />
-			<div>
+		<nav className="min-w-[100vw] min-h-20 flex flex-nowrap justify-between items-center border-b border-white border-opacity-25 fixed">
+			<LogoText
+				className={
+					"cursor-pointer mx-6 min-h-9 max-h-9 min-w-28 max-w-28"
+				}
+			/>
+			<div className="hidden lg:block">
 				<NavButton innerText="Home"></NavButton>
 				<NavButton innerText="Features"></NavButton>
 				<NavButton innerText="How it Works"></NavButton>
 				<NavButton innerText="Pricing"></NavButton>
 			</div>
 			<div>
-			<AuthButtons innerText="Join" />
-			<AuthButtons innerText='Sign in' />
+				<AuthButtons innerText="Join" />
+				<AuthButtons innerText="Sign in" />
+				<Drawer>
+					<DrawerTrigger>Open</DrawerTrigger>
+					<DrawerContent className="absolute translate-y-[-2000px]">
+						<DrawerHeader>
+							<DrawerTitle>Are you absolutely sure?</DrawerTitle>
+							<DrawerDescription>
+								This action cannot be undone.
+							</DrawerDescription>
+						</DrawerHeader>
+						<DrawerFooter>
+							<Button>Submit</Button>
+							<DrawerClose>
+								<Button variant="outline">Cancel</Button>
+							</DrawerClose>
+						</DrawerFooter>
+					</DrawerContent>
+				</Drawer>
 			</div>
 		</nav>
 	);
