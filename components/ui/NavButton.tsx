919afc108038643663dcd6b57c622f8761399f8c
Saxon Payne <saxattack101@gmail.com>
2024-04-16 19:31:49 -0700

    feat: 🚀 Updated NavButton component to support dynamic width and improved hover effects

--- components/ui/NavButton.tsx
+++ components/ui/NavButton.tsx
@@ -8,8 +8,8 @@ type props = {
 const NavButton = ({ innerText }: props) => {
 	return (
 		<Button
-			variant="ghost"
-			className="text-sm mx-3 h-8 font-normal z-10 text-white shadow-2xl hover:text-white hover:bg-slate-300 bg-opacity-100 hover:bg-opacity-45"
+		variant="ghost"
+			className="text-sm mx-6 h-8 w-[auto-10px] hover:w-auto font-normal z-10 text-white shadow-2xl hover:text-white hover:bg-slate-300 bg-opacity-100 hover:bg-opacity-45"
 		>
 			{innerText}
 		</Button>
